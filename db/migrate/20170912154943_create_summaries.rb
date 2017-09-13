63f8a83e94d5d0a3c17163b83cba7256c1540b57
Tyler Calkins <atcalkins@outlook.com>
2017-09-13 11:10:47 -0400

    fix syntax error in summaries migration

--- db/migrate/20170912154943_create_summaries.rb
+++ db/migrate/20170912154943_create_summaries.rb
@@ -1,7 +1,7 @@
 class CreateSummaries < ActiveRecord::Migration[5.1]
   def change
     create_table :summaries do |t|
-      t.string :username, unique true
+      t.string :username, unique: true
       t.jsonb :user_response
       t.jsonb :repos_response
       t.timestamps
